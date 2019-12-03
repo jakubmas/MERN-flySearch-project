a1c150fae9002a19c0fef29625bc50f6a031c58a
Jakub Masłowski <jak.maslowski@gmail.com>
2019-12-03 18:04:36 +0100

    JWT implemented

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const {check, validationResult} = require("express-validator");
 
 const User = require("../models/User");
@@ -24,16 +26,37 @@ router.post(
       return res.status(400).json({errors: errors.array()});
     }
     const {name, email, password} = req.body;
+
     try {
       let user = await User.findOne({email});
       if (user) {
         res.status(400).json({errors: [{msg: "User already exists"}]});
       }
+
       user = new User({name, email, password});
+
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
-      res.send("User registered");
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {expiresIn: 3600000},
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        },
+      );
+
+      //   res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
