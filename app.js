9da8a4975ce7b6ccf4be15b415a468b839e8723c
Jakub Masłowski <jak.maslowski@gmail.com>
2019-12-03 17:50:43 +0100

    Added: routes, db. Register user functionality, DB connected

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 
-const PORT = 666;
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
+// Define Routes
 app.use("/api", require("./routes/flightSearchInput"));
+app.use("/api/users", require("./routes/users"));
+
+const PORT = 666;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
